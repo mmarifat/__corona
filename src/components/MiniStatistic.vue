993fff6e83848ff476ba85923d9cfa220a44cdc0
mmarifat <mma.rifat66@gmail.com>
2020-06-09 00:10:30 +0600

    Update 9 June

---
+++ src/components/MiniStatistic.vue
@@ -0,0 +1,41 @@
+<template>
+	<v-card>
+		<v-card-text class="pa-0">
+			<v-container class="pa-0">
+				<div class="layout row ma-0">
+					<div class="sm6 xs6 flex text-sm-center py-3" :class="color">
+						<div class="headline">{{ title }}</div>
+						<span class="caption">{{ subTitle }}</span>
+					</div>
+					<div class="sm6 xs6 flex" :class="background">
+						<div class="layout column ma-0 justify-center align-center">
+							<v-icon size="40px" class="mt-2" :color="color">{{ icon }}</v-icon>
+						</div>
+					</div>
+				</div>
+			</v-container>
+		</v-card-text>
+	</v-card>
+</template>
+
+<script lang='ts'>
+	import {Component, Prop, Vue} from "vue-property-decorator";
+
+	@Component
+	export default class MiniStatistic extends Vue {
+		@Prop()
+		icon: string
+		@Prop()
+		title: string
+		@Prop()
+		subTitle: string
+		@Prop()
+		color: string
+
+		background: string = 'grey darken-4'
+	}
+</script>
+
+<style lang='scss'>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,156 +1,118 @@
 <template>
-	<v-container fluid>
-		<v-container>
-			<v-row>
-				<v-col cols="8">
-					<v-img class="img-center" src="images/covid-19.svg" max-height="80%" max-width="400px"
-							 lazy-src="images/loading.gif"
-							 alt="corona"/>
-				</v-col>
-				<v-col cols="4" class="mt-10">
-					<v-toolbar color="transparent" class="mt-2" flat>
-						<v-autocomplete v-model="select" :loading="loading" :items="cacheCountries"
-											 :search-input.sync="search" cache-items class="mx-4"
-											 @input="dataLoad" item-value="name" item-text="name"
-											 flat hide-no-data hide-details solo-inverted clearable
-											 label="Type country name!"/>
-						<i :class="countryFlag"/>
-					</v-toolbar>
-				</v-col>
-			</v-row>
-		</v-container>
-		<v-row v-if="!!coronaCountries">
-			<v-spacer/>
-			<v-col md="6">
-				<v-card flat color="transparent">
-					<v-card-text>
-						<br/>
-						<br/>
-						<h1 style="font-size: 40px" class="text-uppercase">{{coronaCountries.country ?
-							coronaCountries.country: 'GLOBAL'}}</h1>
-						<br/>
-						<h3>Last Update: <br/>{{timeConverter(coronaCountries.updated)}}</h3><br/>
-					</v-card-text>
-				</v-card>
-			</v-col>
-			<v-col md="4">
-				<v-img v-if="!!coronaCountries.countryInfo" class="img-right" :src="coronaCountries.countryInfo.flag"
-						 lazy-src="images/loading.gif" alt="flag"/>
-				<v-img v-else width="350px" class="img-right" src="images/globe.svg" lazy-src="images/loading.gif"
-						 alt="flag"/>
-			</v-col>
-			<v-spacer/>
-		</v-row>
-		<v-row class="mt-5">
-			<v-col md="3">
-				<v-card shaped hover color="orange lighten-3">
-					<v-card-title>
-						CASES
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.cases}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-title>
-						TODAY'S CASES
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.todayCases}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-title>
-						RATE (/million)
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.casesPerOneMillion}}</h1>
-					</v-card-text>
-				</v-card>
-			</v-col>
-			<v-col md="3">
-				<v-card shaped hover color="red lighten-1">
-					<v-card-title>
-						DEATHS
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.deaths}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-title>
-						TODAY'S DEATHS
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.todayDeaths}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-title>
-						RATE (/million)
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.deathsPerOneMillion}}</h1>
-					</v-card-text>
-				</v-card>
-			</v-col>
-			<v-col md="3">
-				<v-card shaped hover color="yellow lighten-1">
-					<v-card-title>
-						CRITICAL
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.critical}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-title>
-						ACTIVE
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.active}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-title>
-						RECOVERED
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.recovered}}</h1>
-					</v-card-text>
-				</v-card>
-			</v-col>
-			<v-col md="3">
-				<v-card shaped hover color="green lighten-2">
-					<v-card-title>
-						TESTS
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.tests}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-title>
-						TEST RATE ( /million)
-					</v-card-title>
-					<v-card-text class="mx-10">
-						<h1>{{coronaCountries.testsPerOneMillion}}</h1>
-					</v-card-text>
-					<v-divider/>
-					<v-card-text class="text-center">
-						<br/><br/><br/>
-					</v-card-text>
-				</v-card>
-			</v-col>
-		</v-row>
-		<chart-info/>
-	</v-container>
+	<section class="pt-8 pa-md-12 pa-lg-12 ml-md-12 mr-md-12 ml-lg-12 mr-lg-12">
+		<v-layout row wrap>
+			<v-flex xs12 sm12 md6 lg6 class="pt-1">
+				<v-img class="img-center" src="images/covid-19.svg" max-height="90%" max-width="400px"
+				       alt="corona">
+					<template v-slot:placeholder>
+						<v-row class="fill-height ma-0" align="center" justify="center">
+							<v-progress-circular indeterminate :size="70" :width="8" color="blue"/>
+						</v-row>
+					</template>
+				</v-img>
+			</v-flex>
+			<v-flex xs12 sm12 md6 lg6 class="pt-12">
+				<v-toolbar color="transparent" flat>
+					<v-autocomplete v-model="select" :loading="loading" :items="cacheCountries"
+					                :search-input.sync="search" cache-items class="mx-4"
+					                @input="dataLoad" item-value="name" item-text="name"
+					                flat hide-no-data hide-details solo-inverted clearable
+					                label="Type country name!"/>
+					<i :class="countryFlag"/>
+				</v-toolbar>
+			</v-flex>
+		</v-layout>
+		<v-card color="transparent" v-if="!!coronaCountries" flat>
+			<v-layout row wrap v-if="!!coronaCountries" class="pt-5">
+				<v-flex xs12 sm12 md6 lg6>
+					<v-card flat class="ma-3" color="transparent">
+						<v-card-text class="mt-5">
+							<h2 class="font-weight-bold text-uppercase">{{coronaCountries.country ? coronaCountries.country : 'GLOBAL'}}</h2>
+							<h4>{{coronaCountries.country ? '( '+coronaCountries.continent+' )' : ''}}</h4>
+							<div class="grey--text mt-5">Last Update: {{timeConverter(coronaCountries.updated)}}</div>
+						</v-card-text>
+					</v-card>
+				</v-flex>
+				<v-flex xs12 sm12 md6 lg6>
+					<v-img v-if="!!coronaCountries.countryInfo" class="img-right" :src="coronaCountries.countryInfo.flag" alt="flag">
+						<template v-slot:placeholder>
+							<v-row class="fill-height ma-0" align="center" justify="center">
+								<v-progress-circular indeterminate :size="70" :width="8" color="blue"/>
+							</v-row>
+						</template>
+					</v-img>
+					<v-img v-else width="350px" height="150px" class="img-right" src="images/globe.svg" alt="flag">
+						<template v-slot:placeholder>
+							<v-row class="fill-height ma-0" align="center" justify="center">
+								<v-progress-circular indeterminate :size="70" :width="8" color="blue"/>
+							</v-row>
+						</template>
+					</v-img>
+				</v-flex>
+			</v-layout>
+			<v-layout row wrap class="mb-5 mt-5" justify-center>
+				<v-flex xs12 sm12 md6 lg6>
+					<HighCharts :options="chartTodayOptions"/>
+				</v-flex>
+				<v-flex xs12 sm12 md6 lg6>
+					<HighCharts :options="chartTotalOptions"/>
+				</v-flex>
+			</v-layout>
+			<v-tabs v-model="tab" color="green" grow background-color="transparent">
+				<v-tab v-for="item in tabItems" :key="item">
+					{{ item }}
+				</v-tab>
+			</v-tabs>
+			<v-tabs-items v-model="tab">
+				<v-tab-item v-for="item in tabItems" :key="item" color="transparent">
+					<v-card flat style="background-color: lightcyan">
+						<v-layout row wrap justify-center>
+							<v-flex md6 lg8 sm12 xs12 class="pa-5" v-if="item === 'Cases'">
+								<mini-statistic title="Total" :icon="coronaCountries.cases" color="light-green accent-2" class="mb-5"/>
+								<mini-statistic title="Today" :icon="coronaCountries.todayCases" color="light-green accent-3" class="mb-5"/>
+								<mini-statistic title="Rate / Million" :icon="coronaCountries.casesPerOneMillion" color="light-green accent-4"
+								                :sub-title="'Rate (People)'+coronaCountries.oneCasePerPeople"/>
+							</v-flex>
+							<v-flex md6 lg8 sm12 xs12 class="pa-5" v-if="item === 'Deaths'">
+								<mini-statistic title="Total" :icon="coronaCountries.deaths" color="orange accent-1" class="mb-5"/>
+								<mini-statistic title="Today" :icon="coronaCountries.todayDeaths" color="orange accent-2" class="mb-5"/>
+								<mini-statistic title="Rate / Million" :icon="coronaCountries.deathsPerOneMillion" color="orange accent-3"
+								                :sub-title="'Rate (People)'+coronaCountries.oneDeathPerPeople"/>
+							</v-flex>
+							<v-flex md6 lg8 sm12 xs12 class="pa-5" v-if="item === 'Patients'">
+								<mini-statistic title="Critical" :icon="coronaCountries.critical" color="lime lighten-2" class="mb-5"
+								                :sub-title="'Rate (Million)'+coronaCountries.criticalPerOneMillion"/>
+								<mini-statistic title="Active" :icon="coronaCountries.active" color="lime lighten-1" class="mb-5"
+								                :sub-title="'Rate (Million)'+coronaCountries.activePerOneMillion"/>
+								<mini-statistic title="Recovered" :icon="coronaCountries.recovered" color="lime accent-3"
+								                :sub-title="'Today - '+coronaCountries.todayRecovered+' || Rate (Million) - '+coronaCountries.recoveredPerOneMillion"/>
+							</v-flex>
+							<v-flex md6 lg8 sm12 xs12 class="pa-5" v-if="item === 'Tests'">
+								<mini-statistic title="Population" :icon="coronaCountries.population" color="cyan lighten-2" class="mb-5"/>
+								<mini-statistic title="Total" :icon="coronaCountries.tests" color="cyan lighten-1" class="mb-5"/>
+								<mini-statistic title="Rate (Million)" :icon="coronaCountries.testsPerOneMillion" color="cyan darken-1"
+								                :sub-title="'Rate (People)'+coronaCountries.oneTestPerPeople"/>
+							</v-flex>
+						</v-layout>
+					</v-card>
+				</v-tab-item>
+			</v-tabs-items>
+		</v-card>
+	</section>
 </template>
 
 <script lang="ts">
 	import {Component, Prop, Vue, Watch} from "vue-property-decorator";
-	import ChartInfo from "@/components/ChartInfo.vue";
+	import MiniStatistic from "@/components/MiniStatistic.vue";
+	import {Options} from "highcharts";
+	import {Chart} from "highcharts-vue";
 	import Axios from "axios";
 	import Moment from "moment";
 
 	@Component({
-		components: {ChartInfo}
+		components: {MiniStatistic, HighCharts: Chart}
 	})
 	export default class Home extends Vue {
-		// countries start
 		@Prop({required: true})
 		countries: any;
 
@@ -162,13 +124,8 @@
 
 		countryFlag: string = '';
 
-		changeCountry() {
-			let available = this.countries.find((e: any) => e.name === this.select);
-			if (available)
-				this.countryFlag = 'flag-icon flag flag-icon-' + available.code.toLowerCase();
-			else
-				this.countryFlag = 'fa fa-globe text--secondary';
-		}
+		tab: any = null
+		tabItems: any = ['Cases', 'Deaths', 'Patients', 'Tests']
 
 		@Watch('search')
 		selectCountry(country: string) {
@@ -185,53 +142,164 @@
 			}, 500)
 		}
 
-		// countries end
-
 		timeConverter(time: number) {
 			const fullDate = new Date(time);
 			return Moment(fullDate).format("hh:mm:ss A - ddd MMMDD,YY")
 		}
 
-		// data load
 		coronaCountries: any[] = [];
 
-		mounted() {
+		chartTodayOptions: Options = {}
+		chartTotalOptions: Options = {}
+
+		created() {
 			this.dataLoad()
 		}
 
 		dataLoad() {
+			this.getFromApi().then(() => {
+				let val: any = {}
+				val = this.coronaCountries
+
+				let todaySeries = [{
+					name: 'Cases',
+					data: [val.todayCases]
+				}, {
+					name: 'Critical',
+					data: [val.critical]
+				}, {
+					name: 'Recovered',
+					data: [val.todayRecovered]
+				}, {
+					name: 'Deaths',
+					data: [val.todayDeaths]
+				}]
+
+				let totalSeries = [{
+					name: 'Cases',
+					data: [val.cases]
+				}, {
+					name: 'Active',
+					data: [val.active]
+				}, {
+					name: 'Recovered',
+					data: [val.recovered]
+				}, {
+					name: 'Deaths',
+					data: [val.deaths]
+				}]
+
+				this.chartTodayOptions = {
+					colors: ['#DDDF00', '#ff001e', '#50B432', '#ED561B'],
+					chart: {
+						zoomType: 'x',
+						type: 'column',
+						backgroundColor: 'transparent'
+					},
+					title: {
+						text: null
+					},
+					xAxis: {
+						categories: ['Today']
+					},
+					yAxis: {
+						title: {
+							text: 'In Number'
+						}
+					},
+					legend: {
+						verticalAlign: 'top',
+						enabled: true
+					},
+					plotOptions: {
+						area: {
+							marker: {
+								radius: 2
+							},
+							lineWidth: 1,
+							states: {
+								hover: {
+									lineWidth: 1
+								}
+							},
+							threshold: null
+						}
+					},
+					//@ts-ignore
+					series: todaySeries
+				}
+
+				this.chartTotalOptions = {
+					colors: ['#fae607', '#a36868', '#6407fa', '#fa0707'],
+					chart: {
+						zoomType: 'x',
+						type: 'column',
+						backgroundColor: 'transparent'
+					},
+					title: {
+						text: null
+					},
+					xAxis: {
+						categories: ['Total']
+					},
+					yAxis: {
+						title: {
+							text: 'In Number'
+						}
+					},
+					legend: {
+						verticalAlign: 'top',
+						enabled: true
+					},
+					plotOptions: {
+						area: {
+							marker: {
+								radius: 2
+							},
+							lineWidth: 1,
+							states: {
+								hover: {
+									lineWidth: 1
+								}
+							},
+							threshold: null
+						}
+					},
+					//@ts-ignore
+					series: totalSeries
+				}
+			})
+		}
+
+		getFromApi() {
 			this.coronaCountries = [];
 			if (!this.select) {
-				Axios.get("https://corona.lmao.ninja/all")
-					.then(({data}) => {
-						this.coronaCountries = data
-					});
-				this.countryFlag = 'fa fa-globe text--secondary';
+				return Axios.get("https://corona.lmao.ninja/v2/all")
+					  .then(({data}) => {
+						  this.coronaCountries = data
+					  }).finally(() => {
+						  this.countryFlag = 'fa fa-globe text--secondary';
+					  })
 			} else {
-				Axios.get("https://corona.lmao.ninja/countries/" + this.select)
-					.then(({data}) => {
-						this.coronaCountries = data
-					})
-					.catch(error => {
-						this.$notify({
-							type: 'error',
-							title: "Country's info not found!!"
-						});
-					});
-				let ifAvailable = this.countries.find((e: any) => e.name === this.select);
-				if (ifAvailable)
-					this.countryFlag = 'flag-icon flag-icon-' + ifAvailable.code.toLowerCase();
+				return Axios.get("https://corona.lmao.ninja/v2/countries/" + this.select)
+					  .then(({data}) => {
+						  this.coronaCountries = JSON.parse(JSON.stringify(data))
+					  })
+					  .catch(error => {
+						  this.$notify({
+							  type: 'error',
+							  title: "Country's info not found!!"
+						  });
+					  }).finally(() => {
+						  let ifAvailable = this.countries.find((e: any) => e.name === this.select);
+						  if (ifAvailable)
+							  this.countryFlag = 'flag-icon flag-icon-' + ifAvailable.code.toLowerCase();
+					  })
 			}
 		}
 	}
 </script>
 <style scoped lang="scss">
-	h1 {
-		font-family: "Consolas", serif;
-		font-weight: bolder;
-		font-size: 35px;
-	}
-
 	.img-center {
 		margin: 0 auto;
 		display: block;
@@ -241,5 +309,6 @@
 		float: right;
 		clear: right;
 	}
+
 
 </style>
